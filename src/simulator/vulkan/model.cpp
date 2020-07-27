42db8bf3cd0db34c142a07707749752fd5e5166b
Breno Queiroz <breno1423@gmail.com>
2020-07-27 12:33:01 -0300

    Feat: Ray tracing working

--- src/simulator/vulkan/model.cpp
+++ src/simulator/vulkan/model.cpp
@@ -77,11 +77,11 @@ void Model::loadModel()
 	//---------- Materials ----------//
 	for(const auto& material : objReader.GetMaterials())
 	{
-		Material m = {};
+		//Material m = {};
 
-		m.diffuse = glm::vec4(material.diffuse[0], material.diffuse[1], material.diffuse[2], 1.0);
-		m.diffuseTextureId = -1;
-		//Material m = Material::diffuseLight(glm::vec3(material.diffuse[0], material.diffuse[1], material.diffuse[2]), -1);
+		//m.diffuse = glm::vec4(material.diffuse[0], material.diffuse[1], material.diffuse[2], 1.0);
+		//m.diffuseTextureId = -1;
+		Material m = Material::diffuseLight(glm::vec3(material.diffuse[0], material.diffuse[1], material.diffuse[2]), -1);
 
 		_materials.emplace_back(m);
 	}
@@ -171,71 +171,6 @@ void Model::loadModel()
 			vertex.normal = glm::normalize(vertex.normal);
 		}
 	}
-
-	//tinyobj::attrib_t attrib;
-    //std::vector<tinyobj::shape_t> shapes;
-    //std::vector<tinyobj::material_t> materials;
-    //std::string warn, err;
-
-    //if(!tinyobj::LoadObj(&attrib, &shapes, &materials, &warn, &err, modelPath.c_str()))
-	//{
-	//	std::cout << BOLDRED << "[Model]" << RESET << RED << warn + err << RESET << std::endl;
-	//	exit(1);
-    //}
-
-	//std::unordered_map<Vertex, uint32_t> uniqueVertices{};
-	//for(const auto& shape : shapes)
-	//{
-	//	for(const auto& index : shape.mesh.indices)
-	//	{
-	//		Vertex vertex{};
-
-	//		vertex.pos = {
-	//			attrib.vertices[3 * index.vertex_index + 0],
-	//			attrib.vertices[3 * index.vertex_index + 1],
-	//			attrib.vertices[3 * index.vertex_index + 2]
-	//		};
-
-	//		//vertex.texCoord = {
-	//		//	attrib.texcoords[2 * index.texcoord_index + 0],
-	//		//	attrib.texcoords[2 * index.texcoord_index + 1]
-	//		//};
-	//		vertex.texCoord = {
-	//			attrib.texcoords[2 * index.texcoord_index + 0],
-	//			1.0f - attrib.texcoords[2 * index.texcoord_index + 1]
-	//		};
-
-	//		vertex.color = {1.0f, 1.0f, 1.0f};
-
-	//		_vertices.push_back(vertex);
-
-	//		if (uniqueVertices.count(vertex) == 0) {
-	//			uniqueVertices[vertex] = static_cast<uint32_t>(_vertices.size());
-	//			_vertices.push_back(vertex);
-	//		}
-	//		_indices.push_back(uniqueVertices[vertex]);
-	//	}
-	//}
-
-	//// Materials
-	//for(const auto& material : materials)
-	//{
-	//	Material m{};
-	//	m.diffuse = glm::vec4(material.diffuse[0], material.diffuse[1], material.diffuse[2], 1.0);
-	//	m.diffuseTextureId = -1;
-
-	//	_materials.emplace_back(m);
-	//}
-
-	//if(_materials.empty())
-	//{
-	//	Material m{};
-
-	//	m.diffuse = glm::vec4(0.7f, 0.7f, 0.7f, 1.0);
-	//	m.diffuseTextureId = -1;
-
-	//	_materials.emplace_back(m);
-	//}
 
 	end = std::chrono::steady_clock::now();
 
