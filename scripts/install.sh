3e9803db746c1d5e0649842942933704c1f55e6e
Breno Cunha Queiroz <breno1423@gmail.com>
2021-03-25 23:17:06 -0300

    Test: Build changes to create AUR package

--- scripts/install.sh
+++ scripts/install.sh
@@ -6,6 +6,6 @@ set -e
 
 mkdir --parents build/linux
 cd build/linux
-cmake -D CMAKE_BUILD_TYPE=Release -D VCPKG_TARGET_TRIPLET=x64-linux -D CMAKE_TOOLCHAIN_FILE=../vcpkg.linux/scripts/buildsystems/vcpkg.cmake -D ATTA_GENERATE_PACKAGE_INSTALL=1 ../.. 
+cmake -D CMAKE_BUILD_TYPE=Release -D ATTA_GENERATE_PACKAGE_INSTALL=1 ../.. 
 make -j
 make install
