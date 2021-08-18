bc4df60d61092517b9e6496e0ce58eecf16542b5
Brenocq <breno1423@gmail.com>
2021-08-18 11:19:09 -0300

    Fix: Clang stringId errors

--- src/atta/core/stringId.cpp
+++ src/atta/core/stringId.cpp
@@ -25,27 +25,4 @@ namespace atta
 	{
 		return _id;	
 	}
-
-	constexpr hashId StringId::crc32b(const char* str)
-	{
-		// Reference: Book Hacker's Delight 2nd Edition
-		const unsigned char* message = reinterpret_cast<const unsigned char*>(str);
-		int i = 0, j = 0;
-		hashId byte = 0, crc = 0xFFFFFFFF, mask = 0;
-
-		while(message[i] != 0)
-		{
-			// Get next byte.
-			byte = message[i];            
-			crc = crc ^ byte;
-			for(j = 7; j >= 0; j--)
-			{
-				// Do eight times.
-				mask = -(crc & 1);
-				crc = (crc >> 1) ^ (0xEDB88320 & mask);
-			}
-			i = i + 1;
-		}
-		return ~crc;
-	}
 }
--- src/atta/core/stringId.h
+++ src/atta/core/stringId.h
@@ -20,7 +20,28 @@ namespace atta
 			const std::string& getString();
 			uint32_t getId();
 
-			static constexpr hashId crc32b(const char* str);
+			static constexpr hashId crc32b(const char* str)
+			{
+				// Reference: Book Hacker's Delight 2nd Edition
+				const unsigned char* message = (const unsigned char*)(str);
+				int i = 0, j = 0;
+				hashId byte = 0, crc = 0xFFFFFFFF, mask = 0;
+
+				while(message[i] != 0)
+				{
+					// Get next byte.
+					byte = message[i];            
+					crc = crc ^ byte;
+					for(j = 7; j >= 0; j--)
+					{
+						// Do eight times.
+						mask = -(crc & 1);
+						crc = (crc >> 1) ^ (0xEDB88320 & mask);
+					}
+					i = i + 1;
+				}
+				return ~crc;
+			}
 		
 		private:
 			hashId _id;
@@ -37,4 +58,4 @@ namespace atta
 	}
 }
 
-#endif ATTA_CORE_STRING_ID_H
+#endif// ATTA_CORE_STRING_ID_H
