5062be01225369834aa9518b89ba7e4b6cb54245
Brenocq <breno1423@gmail.com>
2021-09-13 07:32:29 -0300

    Fix: Windows path to char* conversion

--- src/atta/resourceSystem/resources/mesh.cpp
+++ src/atta/resourceSystem/resources/mesh.cpp
@@ -21,7 +21,7 @@ namespace atta
 	void Mesh::loadMesh()
 	{
 		Assimp::Importer importer;
-		const aiScene *scene = importer.ReadFile(fs::absolute(_filename), aiProcess_Triangulate | aiProcess_FlipUVs); 
+		const aiScene *scene = importer.ReadFile(fs::absolute(_filename).string().c_str(), aiProcess_Triangulate | aiProcess_FlipUVs); 
 
 		if(!scene || scene->mFlags & AI_SCENE_FLAGS_INCOMPLETE || !scene->mRootNode)
 		{
