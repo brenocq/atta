7d2507f673d4aa172d19628f0d31400a2e37b35f
brenocq <breno1423@gmail.com>
2021-12-24 21:46:29 -0300

    Test: Button workflow

--- .github/scripts/updateReadmeButtons/decrypt_secret.sh
+++ .github/scripts/updateReadmeButtons/decrypt_secret.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 # Decrypt the file
-mkdir $HOME/secrets
 gpg --quiet --batch --yes --decrypt --passphrase="$SECRET_GCLOUD" \
---output $HOME/secrets/secret.json secret.json.gpg
+--output secret.json secret.json.gpg
+echo `pwd`/secret.json
