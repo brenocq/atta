e475ea24e727b94e6c5bf2591f38aefadaf3184c
brenocq <breno1423@gmail.com>
2022-06-08 09:24:30 +0200

    Refactor (IO): Http change from cpprestsdk to cppnetlib #14

---
+++ src/atta/ioSystem/http/httpCppNetlib.cpp
@@ -0,0 +1,41 @@
+//--------------------------------------------------
+// Atta IO System
+// httpCppNetlib.cpp
+// Date: 2022-06-07
+// By Breno Cunha Queiroz
+//--------------------------------------------------
+// The .cpp file with the http implementation is chosen at compile time
+
+#include <atta/ioSystem/http/http.h>
+#include <boost/network/protocol/http/client.hpp>
+using namespace boost::network;
+
+namespace atta::io
+{
+    const std::string Http::_implType = "cppnetlib";
+
+    Http::Http(Http::CreateInfo info)
+    {
+
+    }
+
+    Http::~Http()
+    {
+
+    }
+
+    Http::Content Http::get(Http::Content content)
+    {
+
+    }
+    
+    Http::Content Http::post(Http::Content content)
+    {
+
+    }
+
+    Http::Content Http::put(Http::Content content)
+    {
+
+    }
+}
