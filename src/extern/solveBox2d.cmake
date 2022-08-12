4384f3cfecde12ddba35f5bfe59adf73a87994c9
brenocq <breno1423@gmail.com>
2022-08-12 07:46:59 +0200

    Chore: Download bullet release from atta-deps

--- src/extern/solveBox2d.cmake
+++ src/extern/solveBox2d.cmake
@@ -1,17 +1,17 @@
 set(ATTA_BOX2D_SUPPORT FALSE)
 set(ATTA_BOX2D_TARGETS "")
 
-find_package(box2d)
-if(box2d_FOUND)
-    atta_log(Success Extern "Box2D support (installed)")
-    set(ATTA_BOX2D_TARGETS box2d::box2d)
-    set(ATTA_BOX2D_SUPPORT TRUE)
-
-    # Fix box2d include
-    get_property(fix_box2d_includes TARGET ${ATTA_BOX2D_TARGETS} PROPERTY INTERFACE_INCLUDE_DIRECTORIES)
-    list(REMOVE_ITEM fix_box2d_includes "/usr/local/src/extern")
-    set_property(TARGET ${ATTA_BOX2D_TARGETS} PROPERTY INTERFACE_INCLUDE_DIRECTORIES ${fix_box2d_includes})
-else()
+#find_package(box2d)
+#if(box2d_FOUND)
+#    atta_log(Success Extern "Box2D support (installed)")
+#    set(ATTA_BOX2D_TARGETS box2d::box2d)
+#    set(ATTA_BOX2D_SUPPORT TRUE)
+#
+#    # Fix box2d include
+#    get_property(fix_box2d_includes TARGET ${ATTA_BOX2D_TARGETS} PROPERTY INTERFACE_INCLUDE_DIRECTORIES)
+#    list(REMOVE_ITEM fix_box2d_includes "/usr/local/src/extern")
+#    set_property(TARGET ${ATTA_BOX2D_TARGETS} PROPERTY INTERFACE_INCLUDE_DIRECTORIES ${fix_box2d_includes})
+#else()
     set(BOX2D_BUILD_UNIT_TESTS OFF CACHE INTERNAL "" FORCE)
     set(BOX2D_BUILD_TESTBED OFF CACHE INTERNAL "" FORCE)
     FetchContent_Declare(
@@ -30,4 +30,4 @@ else()
     atta_log(Success Extern "Box2D support (source)")
     set(ATTA_BOX2D_SUPPORT TRUE)
     set(ATTA_BOX2D_TARGETS box2d)
-endif()
+#endif()
--- src/extern/solveBullet.cmake
+++ src/extern/solveBullet.cmake
@@ -1,26 +1,26 @@
 set(ATTA_BULLET_SUPPORT FALSE)
 set(ATTA_BULLET_TARGETS "")
 
-find_package(Bullet QUIET)
-if(Bullet_FOUND)
-    set(ATTA_BULLET_TARGETS ${BULLET_LIBRARIES})
-    atta_add_include_dirs(${BULLET_INCLUDE_DIR})
-
-    atta_log(Success Extern "Bullet support (installed)")
-    set(ATTA_BULLET_SUPPORT TRUE)
-else()
+#if(Bullet_FOUND)
+#    set(ATTA_BULLET_TARGETS ${BULLET_LIBRARIES})
+#    atta_add_include_dirs(${BULLET_INCLUDE_DIR})
+#
+#    atta_log(Success Extern "Bullet support (installed)")
+#    set(ATTA_BULLET_SUPPORT TRUE)
+#else()
     set(USE_GRAPHICAL_BENCHMARK OFF CACHE INTERNAL "" FORCE)
     set(BUILD_UNIT_TESTS OFF CACHE INTERNAL "" FORCE)
-    set(INSTALL_CMAKE_FILES OFF CACHE INTERNAL "" FORCE)
     set(BUILD_BULLET3 ON CACHE INTERNAL "" FORCE)
-    set(BUILD_BULLET2_DEMOS OFF CACHE INTERNAL "" FORCE)
     set(BUILD_OBJ2SDF_EXTRA OFF CACHE INTERNAL "" FORCE)
+    set(BUILD_BULLET2_DEMOS OFF CACHE INTERNAL "" FORCE)
+    set(BUILD_CPU_DEMOS OFF CACHE INTERNAL "" FORCE)
+    set(BUILD_EXTRAS OFF CACHE INTERNAL "" FORCE)
+    set(INSTALL_LIBS OFF CACHE INTERNAL "" FORCE)
+    set(INSTALL_CMAKE_FILES OFF CACHE INTERNAL "" FORCE)
+
     FetchContent_Declare(
         bullet3
-        GIT_REPOSITORY "https://github.com/bulletphysics/bullet3"
-        GIT_TAG "3.24"
-        GIT_PROGRESS TRUE
-        GIT_SHALLOW TRUE
+        URL "https://storage.googleapis.com/atta-deps/bullet3-3.24-light.zip"
     )
     atta_log(Info Extern "Fetching Bullet...")
     FetchContent_MakeAvailable(bullet3)
@@ -31,4 +31,4 @@ else()
 
     atta_log(Success Extern "Bullet support (source)")
     set(ATTA_BULLET_SUPPORT TRUE)
-endif()
+#endif()
