4fdb0651d4882d5454d7369d71a068b5def4e28e
Breno Cunha Queiroz <breno1423@gmail.com>
2025-01-15 19:31:22 +0100

    Chore: Fix curlpp build

--- src/extern/solveCurlpp.cmake
+++ src/extern/solveCurlpp.cmake
@@ -7,9 +7,8 @@ if(ATTA_SYSTEM_NAME MATCHES "Linux")
         FetchContent_Declare(
             curlpp
             GIT_REPOSITORY "https://github.com/jpbarrette/curlpp"
-            GIT_TAG "v0.8.1"
+            GIT_TAG "d080e301ec71061c46049d23fedfb8106be3153f"
             GIT_PROGRESS TRUE
-            GIT_SHALLOW TRUE
         )
         atta_log(Info Extern "Fetching curlpp...")
         atta_FetchContent_MakeAvailable(curlpp)
