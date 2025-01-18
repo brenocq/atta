dee8723200813d6eff930f824423ecbdea2c1681
Breno Cunha Queiroz <breno1423@gmail.com>
2025-01-18 15:39:02 +0100

    Chore: Fix web build

--- src/extern/solveResources.cmake
+++ src/extern/solveResources.cmake
@@ -1,8 +1,8 @@
-#FetchContent_Declare(resources
-#    URL "https://atta-resources.s3.amazonaws.com/v0.3.1-ui-temp.zip"
-#    SOURCE_DIR ${CMAKE_CURRENT_SOURCE_DIR}/resources
-#)
-#
-#atta_log(Info Extern "Downloading resources...")
-#FetchContent_Populate(resources)
-#atta_log(Success Extern "Resources downloaded")
+FetchContent_Declare(resources
+    URL "https://atta-resources.s3.amazonaws.com/v0.3.1-ui-temp.zip"
+    SOURCE_DIR ${CMAKE_CURRENT_SOURCE_DIR}/resources
+)
+
+atta_log(Info Extern "Downloading resources...")
+FetchContent_Populate(resources)
+atta_log(Success Extern "Resources downloaded")
