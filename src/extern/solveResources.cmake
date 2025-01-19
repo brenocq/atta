d1ead43c0f1b00c46a6f21f091efd844a999f7dd
Breno Cunha Queiroz <breno1423@gmail.com>
2025-01-19 13:19:57 +0100

    Chore: Update resources link

--- src/extern/solveResources.cmake
+++ src/extern/solveResources.cmake
@@ -1,5 +1,5 @@
 FetchContent_Declare(resources
-    URL "https://atta-resources.s3.amazonaws.com/v0.3.1-ui-temp.zip"
+    URL "https://atta-resources.s3.amazonaws.com/v0.3.2.zip"
     SOURCE_DIR ${CMAKE_CURRENT_SOURCE_DIR}/resources
 )
 
