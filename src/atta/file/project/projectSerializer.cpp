9dca9a0b24fe7f82561f4465eee85d84ebc4bb3e
Breno Cunha Queiroz <breno1423@gmail.com>
2025-02-26 11:00:56 +0100

    Feat: Deserialize entity components

--- src/atta/file/project/projectSerializer.cpp
+++ src/atta/file/project/projectSerializer.cpp
@@ -11,6 +11,7 @@
 
 #include <atta/component/base.h>
 #include <atta/component/components/components.h>
+#include <atta/component/components/relationship.h>
 #include <atta/component/interface.h>
 
 #include <atta/resource/interface.h>
@@ -227,6 +228,7 @@ void serializeAttribute(Section& section, const std::string& cmpName, cmp::Compo
             break;
         case cmp::AttributeType::UINT32:
             if (attribute.options.size() > 0)
+                // Serialize uint32_t from enum as string
                 section[dataKey] = std::any_cast<const char*>(attribute.options[*reinterpret_cast<uint32_t*>(ptr)]);
             else
                 section[dataKey] = *reinterpret_cast<uint32_t*>(ptr);
@@ -274,25 +276,34 @@ std::vector<Section> ProjectSerializer::serializeNodes() {
     std::vector<Section> sections;
     std::vector<cmp::EntityId> entities = cmp::getNoCloneView();
     // Serialize each entity as a node
-    for (cmp::EntityId entity : entities) {
+    for (cmp::EntityId eid : entities) {
         Section section("node");
-        section["id"] = entity;
+        section["id"] = eid;
 
         // Serialize components
         for (cmp::ComponentRegistry* compReg : cmp::getComponentRegistries()) {
             // Check if entity has this component
-            cmp::Component* comp = cmp::getComponentById(compReg->getId(), entity);
+            cmp::Component* comp = cmp::getComponentById(compReg->getId(), eid);
             if (comp == nullptr)
                 continue;
             // Serialize each attribute of the component
             std::string cmpName = toCamelCase(compReg->getDescription().name);
 
-            const std::vector<cmp::AttributeDescription> attributeDescriptions = compReg->getDescription().attributeDescriptions;
-            for (size_t i = 0; i < attributeDescriptions.size(); i++) {
-                cmp::AttributeDescription aDesc = attributeDescriptions[i];
-                size_t size = (i == attributeDescriptions.size() - 1) ? compReg->getSizeof() - aDesc.offset
-                                                                      : attributeDescriptions[i + 1].offset - aDesc.offset;
-                serializeAttribute(section, cmpName, comp, aDesc, size);
+            if (cmpName == "relationship") {
+                // TODO implement serialization of custom components
+                cmp::Relationship* rel = cmp::Entity(eid).get<cmp::Relationship>();
+                if (rel == nullptr)
+                    continue;
+                if (rel->getParent() != -1)
+                    section[cmpName + ".parent"] = rel->getParent().getId();
+            } else {
+                const std::vector<cmp::AttributeDescription> attributeDescriptions = compReg->getDescription().attributeDescriptions;
+                for (size_t i = 0; i < attributeDescriptions.size(); i++) {
+                    cmp::AttributeDescription aDesc = attributeDescriptions[i];
+                    size_t size = (i == attributeDescriptions.size() - 1) ? compReg->getSizeof() - aDesc.offset
+                                                                          : attributeDescriptions[i + 1].offset - aDesc.offset;
+                    serializeAttribute(section, cmpName, comp, aDesc, size);
+                }
             }
         }
 
@@ -391,6 +402,165 @@ void ProjectSerializer::deserializeMaterial(const Section& section) {
     res::create<res::Material>(id, material);
 }
 
-void ProjectSerializer::deserializeNode(const Section& section) {}
+#define ATTA_DESERIALIZE_VECTOR(TYPE, CAST_TYPE)                                                                                                     \
+    case cmp::AttributeType::VECTOR_##TYPE: {                                                                                                        \
+        std::vector<CAST_TYPE> vec = std::vector<CAST_TYPE>(section[dataKey]);                                                                       \
+        std::copy(vec.begin(), vec.end(), reinterpret_cast<CAST_TYPE*>(ptr));                                                                        \
+        break;                                                                                                                                       \
+    }
+
+void deserializeAttribute(const Section& section, const std::string& cmpName, cmp::Component* comp, const cmp::AttributeDescription& attribute) {
+    std::string dataKey = cmpName + '.' + toCamelCase(attribute.name);
+    if (!section.contains(dataKey))
+        return;
+
+    uint8_t* ptr = reinterpret_cast<uint8_t*>(comp) + attribute.offset; // Pointer to the attribute
+
+    switch (attribute.type) {
+        case cmp::AttributeType::BOOL:
+            *reinterpret_cast<bool*>(ptr) = bool(section[dataKey]);
+            break;
+        case cmp::AttributeType::CHAR:
+            *reinterpret_cast<char*>(ptr) = char(section[dataKey]);
+            break;
+        case cmp::AttributeType::INT8:
+            *reinterpret_cast<int8_t*>(ptr) = int8_t(section[dataKey]);
+            break;
+        case cmp::AttributeType::INT16:
+            *reinterpret_cast<int16_t*>(ptr) = int16_t(section[dataKey]);
+            break;
+        case cmp::AttributeType::INT32:
+            *reinterpret_cast<int32_t*>(ptr) = int32_t(section[dataKey]);
+            break;
+        case cmp::AttributeType::INT64:
+            *reinterpret_cast<int64_t*>(ptr) = int64_t(section[dataKey]);
+            break;
+        case cmp::AttributeType::UINT8:
+            *reinterpret_cast<uint8_t*>(ptr) = uint8_t(section[dataKey]);
+            break;
+        case cmp::AttributeType::UINT16:
+            *reinterpret_cast<uint16_t*>(ptr) = uint16_t(section[dataKey]);
+            break;
+        case cmp::AttributeType::UINT32:
+            if (!attribute.options.empty()) {
+                // Deserialize uint32_t from enum as string
+                std::string value = std::string(section[dataKey]);
+                auto it = std::find_if(attribute.options.begin(), attribute.options.end(),
+                                       [&](const std::any& a) { return std::any_cast<std::string>(a) == value; });
+                if (it != attribute.options.end())
+                    *reinterpret_cast<uint32_t*>(ptr) = std::distance(attribute.options.begin(), it);
+            } else {
+                *reinterpret_cast<uint32_t*>(ptr) = uint32_t(section[dataKey]);
+            }
+            break;
+        case cmp::AttributeType::UINT64:
+            *reinterpret_cast<uint64_t*>(ptr) = uint64_t(section[dataKey]);
+            break;
+        case cmp::AttributeType::FLOAT32:
+            *reinterpret_cast<float*>(ptr) = float(section[dataKey]);
+            break;
+        case cmp::AttributeType::FLOAT64:
+            *reinterpret_cast<double*>(ptr) = double(section[dataKey]);
+            break;
+        case cmp::AttributeType::VECTOR_CHAR: {
+            std::string value = std::string(section[dataKey]);
+            std::copy(value.begin(), value.end(), reinterpret_cast<char*>(ptr));
+            break;
+        }
+            ATTA_DESERIALIZE_VECTOR(BOOL, bool)
+            ATTA_DESERIALIZE_VECTOR(INT8, int8_t)
+            ATTA_DESERIALIZE_VECTOR(INT16, int16_t)
+            ATTA_DESERIALIZE_VECTOR(INT32, int32_t)
+            ATTA_DESERIALIZE_VECTOR(INT64, int64_t)
+            ATTA_DESERIALIZE_VECTOR(UINT8, uint8_t)
+            ATTA_DESERIALIZE_VECTOR(UINT16, uint16_t)
+            ATTA_DESERIALIZE_VECTOR(UINT32, uint32_t)
+            ATTA_DESERIALIZE_VECTOR(UINT64, uint64_t)
+            ATTA_DESERIALIZE_VECTOR(FLOAT32, float)
+            ATTA_DESERIALIZE_VECTOR(FLOAT64, double)
+        case cmp::AttributeType::QUAT:
+            *reinterpret_cast<quat*>(ptr) = quat(section[dataKey]);
+            break;
+        case cmp::AttributeType::STRINGID:
+            *reinterpret_cast<StringId*>(ptr) = std::string(section[dataKey]);
+            break;
+        case cmp::AttributeType::CUSTOM:
+            // Ignore custom attributes
+            break;
+        default:
+            LOG_WARN("file::ProjectSerializer", "Attribute [w]$0.$1[] has unsupported type [w]$2[]. The attribute will not be deserialized", cmpName,
+                     attribute.name, static_cast<int>(attribute.type));
+            break;
+    }
+}
+
+void ProjectSerializer::deserializeNode(const Section& section) {
+    // Create entity
+    cmp::Entity entity;
+    if (section.contains("id")) {
+        cmp::EntityId id = cmp::EntityId(section["id"]);
+
+        // Check if entity doesn't already exist
+        std::vector<cmp::EntityId> entities = cmp::getEntitiesView();
+        bool alreadyExists = std::find(entities.begin(), entities.end(), id) != entities.end();
+        if (!alreadyExists) {
+            // Create entity if it doesn't exist
+            cmp::EntityId res = cmp::createEntity(id);
+            if (res != id) {
+                LOG_WARN("file::ProjectSerializer", "Could not create entity with id $0", id);
+                return;
+            }
+        }
+
+        entity = id;
+    } else
+        return;
+
+    // Iterate through all registered components
+    for (cmp::ComponentRegistry* compReg : cmp::getComponentRegistries()) {
+        std::string cmpName = toCamelCase(compReg->getDescription().name);
+
+        if (cmpName == "relationship") {
+            // TODO implement serialization of custom components
+            if (section.contains("relationship.parent")) {
+                // Check if parent is valid
+                cmp::EntityId parent = uint32_t(section["relationship.parent"]);
+                if (parent == -1)
+                    continue;
+
+                // Create parent if it doesn't exist yet
+                std::vector<cmp::EntityId> entities = cmp::getEntitiesView();
+                bool parentExists = std::find(entities.begin(), entities.end(), parent) != entities.end();
+                if (!parentExists)
+                    cmp::createEntity(parent);
+
+                // Update relationship component
+                entity.add<cmp::Relationship>()->setParent(parent, entity);
+
+                LOG_DEBUG("file::ProjectSerializer", "--------------> Entity [w]$0[] parent [w]$1", entity, parent);
+            }
+        } else {
+            // Check if this component exists in the serialized section
+            bool hasComponent = false;
+            for (const cmp::AttributeDescription& attribute : compReg->getDescription().attributeDescriptions) {
+                std::string dataKey = cmpName + '.' + toCamelCase(attribute.name);
+                if (section.contains(dataKey)) {
+                    hasComponent = true;
+                    break;
+                }
+            }
+            if (!hasComponent)
+                continue;
+
+            // Add component to entity
+            cmp::Component* comp = cmp::addComponentById(compReg->getId(), entity);
+
+            // Deserialize each attribute
+            for (const cmp::AttributeDescription& attribute : compReg->getDescription().attributeDescriptions) {
+                deserializeAttribute(section, cmpName, comp, attribute);
+            }
+        }
+    }
+}
 
 } // namespace atta::file
