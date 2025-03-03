0ccffe04d525c74db9c3ecb1b49137af5200f224
Breno Cunha Queiroz <breno1423@gmail.com>
2025-03-03 20:55:37 +0100

    Fix: MacOS glfw window

--- src/atta/graphics/windows/glfwWindow.cpp
+++ src/atta/graphics/windows/glfwWindow.cpp
@@ -29,10 +29,16 @@ GlfwWindow::GlfwWindow(const CreateInfo& info) : Window(info) {
         glfwInit();
 
     if (info.useOpenGL) {
-        // glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-        // glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
-        // glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-        // glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // Needed for apple?
+#if defined(ATTA_OS_MACOS)
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+        glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+        glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // Required on macOS
+#else
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+        glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+#endif
         glfwWindowHint(GLFW_CLIENT_API, GLFW_OPENGL_API);
     } else
         glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
