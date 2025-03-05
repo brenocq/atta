60435b94ed20ff179f8aa766705965686fd27a6b
Breno Cunha Queiroz <breno1423@gmail.com>
2025-03-05 12:02:14 +0100

    Style: Remove MacOs comment

--- src/atta/graphics/windows/glfwWindow.cpp
+++ src/atta/graphics/windows/glfwWindow.cpp
@@ -35,7 +35,7 @@ GlfwWindow::GlfwWindow(const CreateInfo& info) : Window(info) {
         glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
         glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
         glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-        glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // Required on macOS
+        glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 #else
         glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
         glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
