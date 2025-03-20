596e7c507b75987334bb8ddb60d159e8abc69d57
Breno Cunha Queiroz <breno1423@gmail.com>
2025-03-20 21:41:12 +0100

    Fix: Script registration for static projects

--- src/atta/script/interface.h
+++ src/atta/script/interface.h
@@ -22,9 +22,25 @@ std::vector<StringId> getScriptSids();
 ProjectScript* getProjectScript();
 StringId getProjectScriptSid();
 
+//----- Script Registration -----//
 #ifdef ATTA_STATIC_PROJECT
 ProjectScript* registerProjectScript(std::string name, ProjectScript* projectScript);
 Script* registerScript(std::string name, Script* script);
+#endif
+
+#ifdef ATTA_STATIC_PROJECT
+template <typename T>
+class ScriptRegistration {
+    static Script* reg;
+};
+#define ATTA_REGISTER_SCRIPT(TYPE)                                                                                                                   \
+    template <>                                                                                                                                      \
+    inline ::atta::script::Script* ::atta::script::ScriptRegistration<TYPE>::reg = ::atta::script::registerScript(#TYPE, new TYPE());
+#else
+#define ATTA_REGISTER_SCRIPT(TYPE)                                                                                                                   \
+    extern "C" {                                                                                                                                     \
+    std::pair<const char*, atta::script::Script*> createScript() { return {#TYPE, static_cast<atta::script::Script*>(new TYPE())}; }                 \
+    }
 #endif
 
 } // namespace atta::script
