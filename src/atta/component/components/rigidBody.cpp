b310120c604ed060f24990c21521fc5cf848116b
Breno Cunha Queiroz <breno1423@gmail.com>
2025-04-11 13:47:42 +0200

    Fix: Component float vector serialization

--- src/atta/component/components/rigidBody.cpp
+++ src/atta/component/components/rigidBody.cpp
@@ -11,8 +11,8 @@ template <>
 ComponentDescription& TypedComponentRegistry<RigidBody>::getDescription() {
     static ComponentDescription desc = {"Rigid Body",
                                         {{AttributeType::UINT32, offsetof(RigidBody, type), "type", {}, {}, {}, {"DYNAMIC", "KINEMATIC", "STATIC"}},
-                                         {AttributeType::FLOAT32, offsetof(RigidBody, linearVelocity), "linearVelocity", {}, {}, 0.001f},
-                                         {AttributeType::FLOAT32, offsetof(RigidBody, angularVelocity), "angularVelocity", {}, {}, 0.001f},
+                                         {AttributeType::VECTOR_FLOAT32, offsetof(RigidBody, linearVelocity), "linearVelocity", {}, {}, 0.001f},
+                                         {AttributeType::VECTOR_FLOAT32, offsetof(RigidBody, angularVelocity), "angularVelocity", {}, {}, 0.001f},
                                          {AttributeType::FLOAT32, offsetof(RigidBody, mass), "mass", 0.0f, 100.0f, 0.001f},
                                          {AttributeType::FLOAT32, offsetof(RigidBody, friction), "friction", 0.0f, 1.0f, 0.001f},
                                          {AttributeType::FLOAT32, offsetof(RigidBody, restitution), "restitution", 0.0f, 1.0f, 0.001f},
